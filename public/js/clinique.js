60df712c4b006afe7e4d9d6af070a60886ba67bf
Saly6C <salimatacisse59@gmail.com>
2019-10-29 23:52:17 +0100

    Sous-menu opérations ajoutés

--- public/js/clinique.js
+++ public/js/clinique.js
@@ -1,4 +1,24 @@
-$( ".fa-angle-right" ).on( "click", function() {
-    $('#menu-consultation').slideDown();
-    // $("#menu-consultation").css('display','block');
-  });
+$(".consultation").on( "click", function() {
+    $('#operationsConsultation').toggle(1000);
+    $(this).find("i").toggleClass("fa-angle-right fa-angle-down");
+});
+
+$(".hospitalisation").on( "click", function() {
+    $('#operationsHospitalisation').toggle(1000);
+    $(this).find("i").toggleClass("fa-angle-right fa-angle-down");
+});
+
+$(".dossierPatient").on( "click", function() {
+  $('#operationsDossierPatient').toggle(1000);
+  $(this).find("i").toggleClass("fa-angle-right fa-angle-down");
+});
+
+$(".patient").on( "click", function() {
+  $('#operationsPatient').toggle(1000);
+  $(this).find("i").toggleClass("fa-angle-right fa-angle-down");
+});
+
+$(".parametre").on( "click", function() {
+  $('#operationsParametre').toggle(1000);
+  $(this).find("i").toggleClass("fa-angle-right fa-angle-down");
+});
