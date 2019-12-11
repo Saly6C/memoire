b5a5472d659b04575073ff34deff755b8f12bfac
Saly6C <salimatacisse59@gmail.com>
2019-12-11 21:18:49 +0000

    methode souscription et login crééé

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,29 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\SouscriptionType;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class SecurityController extends AbstractController {
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/souscription", name="security_souscription")
+     */
+    public function souscription(Request $request, ManagerRegistry $manager, UserPasswordEncoderInterface $encoder)
+    {
+        //on créé l'objet user qu'on va lier à notre formulaire
+        $user = new User();
+        //ici on intancie notre formulaire 
+        $form = $this->createForm(SouscriptionType::class,$user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())  {
+            $hashpdw = $encoder->encodePassword($user,$user->getPassword());
+            $user->setPassword($hashpdw);
+            $mr =  $manager->getManager();
+            $mr->persist($user);
+            $mr->flush();
+            return $this->redirectToRoute('clinic_login');
+        }
+        return $this->render('security/souscription.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 
     /**
      * @Route("/login", name="clinic_login")
      */
-    public function login(Request $request, AuthenticationUtils $auth) {
-        // get the login error if there is one
-        $error = $auth->getLastAuthenticationError();
+    public function login() {
+        return $this->render('security/login.html.twig');
+    }
 
-        var_dump($error);
-        // last username entered by the user
-        $lastUsername = $auth->getLastUsername();
-        var_dump($lastUsername);
-        return $this->render('security/login.html.twig', array(
-            'lastUsername' => $lastUsername,
-            'error' => $error
-        ));
+    /**
+     * @Route("/logout", name= "clinic_logout")
+     */
+    public function logout() {
 
-        
     }
 }
