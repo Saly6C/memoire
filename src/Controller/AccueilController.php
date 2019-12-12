fd047a6abf7772339a1d8db658b0e38f14e159a1
Saly6C <salimatacisse59@gmail.com>
2019-12-12 19:32:14 +0000

    methode connexion ajoutée temporairement, nom du rendu changé en dashboard

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,10 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController {
 
+
+    /**
+     * @Route("/", name = "clinic_accueil")
+     */
+    public function connexion() {
+        return $this->render("/accueil/accueilConnexion.html.twig");
+    }
     /**
      * @Route("/dashboard", name= "clinic_dashboard")
      */
     public function index() {
-        return $this->render("/accueil/index.html.twig");
+        return $this->render("/accueil/dashboard.html.twig");
     }
 }
