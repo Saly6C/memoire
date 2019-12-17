a9e442e02097398b3b6ecd00f525f90e2b599e36
Saly6C <salimatacisse59@gmail.com>
2019-12-17 21:09:29 +0000

    evenement en stand-by

--- public/js/clinique.js
+++ public/js/clinique.js
@@ -1,24 +1,24 @@
 $(".consultation").on( "click", function() {
     $('#operationsConsultation').toggle(1000);
-    $(this).find($(".test")).toggleClass("fa-angle-right fa-angle-down");
+    // $(this).find($(".test")).toggleClass("fa-angle-right fa-angle-down");
 });
 
-$(".hospitalisation").on( "click", function() {
-    $('#operationsHospitalisation').toggle(1000);
-    $(this).find($(".test")).toggleClass("fa-angle-right fa-angle-down");
-});
+// $(".hospitalisation").on( "click", function() {
+//     $('#operationsHospitalisation').toggle(1000);
+//     $(this).find($(".test")).toggleClass("fa-angle-right fa-angle-down");
+// });
 
-$(".dossierPatient").on( "click", function() {
-  $('#operationsDossierPatient').toggle(1000);
-  $(this).find($(".test")).toggleClass("fa-angle-right fa-angle-down");
-});
+// $(".dossierPatient").on( "click", function() {
+//   $('#operationsDossierPatient').toggle(1000);
+//   $(this).find($(".test")).toggleClass("fa-angle-right fa-angle-down");
+// });
 
-$(".patient").on( "click", function() {
-  $('#operationsPatient').toggle(1000);
-  $(this).find($(".test")).toggleClass("fa-angle-right fa-angle-down");
-});
+// $(".patient").on( "click", function() {
+//   $('#operationsPatient').toggle(1000);
+//   $(this).find($(".test")).toggleClass("fa-angle-right fa-angle-down");
+// });
 
-$(".parametre").on( "click", function() {
-  $('#operationsParametre').toggle(1000);
-  $(this).find($(".test")).toggleClass("fa-angle-right fa-angle-down");
-});
+// $(".parametre").on( "click", function() {
+//   $('#operationsParametre').toggle(1000);
+//   $(this).find($(".test")).toggleClass("fa-angle-right fa-angle-down");
+// });
