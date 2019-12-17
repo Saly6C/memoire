d86a68b5c2f70d26ceab28af8dd05bd21546b7ce
Saly6C <salimatacisse59@gmail.com>
2019-12-17 21:11:17 +0000

    fonction connexion commentée, chemain de la route mis avant le nom de la classe

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,23 +3,31 @@
 namespace App\Controller;
 
 
+use App\Form\PatientType;
+use App\Repository\PatientRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+     * @Route("/dashboard")
+     */
 class AccueilController extends AbstractController {
 
 
+    // /**
+    //  * @Route("/", name = "clinic_accueil")
+    //  */
+    // public function connexion() {
+    //     return $this->render("/accueil/accueilConnexion.html.twig");
+    // }
     /**
-     * @Route("/", name = "clinic_accueil")
+     * @Route("/", name= "clinic_dashboard")
      */
-    public function connexion() {
-        return $this->render("/accueil/accueilConnexion.html.twig");
-    }
-    /**
-     * @Route("/dashboard", name= "clinic_dashboard")
-     */
-    public function index() {
+    public function index() { 
         return $this->render("/accueil/dashboard.html.twig");
     }
+
+    
 }
