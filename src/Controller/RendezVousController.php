db679c080e7f0c5bda7443b3bfa0bd50d73679bb
Saly6C <salimatacisse59@gmail.com>
2019-12-20 14:58:58 +0000

    C:/Program Files/Git/rendez/vous changé en /dashboard/rendez/vous

--- src/Controller/RendezVousController.php
+++ src/Controller/RendezVousController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/rendez/vous")
+ * @Route("/dashboard/rendez/vous")
  */
 class RendezVousController extends AbstractController
 {
