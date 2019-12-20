d9c9ee4abe18785d9a005acfd860edf339bebfbb
Saly6C <salimatacisse59@gmail.com>
2019-12-20 13:36:41 +0000

    Controller, Formulaires crud, classe PersonnelType générés

---
+++ src/Form/PersonnelType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Personnel;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonnelType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('fonction')
+            ->add('adressePersonnel')
+            ->add('telephonePersonnel')
+            ->add('nomPersonnel')
+            ->add('service')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Personnel::class,
+        ]);
+    }
+}
