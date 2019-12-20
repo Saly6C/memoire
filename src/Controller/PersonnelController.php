64f08b659c112ed0087e65e19b388bced8ef0a0c
Saly6C <salimatacisse59@gmail.com>
2019-12-20 14:03:37 +0000

    path /personnel changé en /dashboard/personnel, formulaires améliorés, balise a ajouté au left menu pour personnel

--- src/Controller/PersonnelController.php
+++ src/Controller/PersonnelController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/personnel")
+ * @Route("/dashboard/personnel")
  */
 class PersonnelController extends AbstractController
 {
