77f5ae71c513a5db86b56a896312f4ffe1aae1be
Saly6C <salimatacisse59@gmail.com>
2019-12-20 16:24:21 +0000

    path /hospitalisation changé en /dashboard/hospitalisation

--- src/Controller/HospitalisationController.php
+++ src/Controller/HospitalisationController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/hospitalisation")
+ * @Route("/dashboard/hospitalisation")
  */
 class HospitalisationController extends AbstractController
 {
