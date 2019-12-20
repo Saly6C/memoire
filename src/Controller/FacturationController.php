58085442e470ff428f7a9cc18f997b9d09833b9b
Saly6C <salimatacisse59@gmail.com>
2019-12-20 16:25:15 +0000

    path /facturation changé en /dashboard/facturation

--- src/Controller/FacturationController.php
+++ src/Controller/FacturationController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/facturation")
+ * @Route("/dashboard/facturation")
  */
 class FacturationController extends AbstractController
 {
