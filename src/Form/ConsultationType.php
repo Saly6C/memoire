776d98639cc4905cf07e26a9a399ae88bfd420b3
Saly6C <salimatacisse59@gmail.com>
2019-12-20 16:21:58 +0000

    Controller, class Form/ConsultationType, formulaires crud générés

---
+++ src/Form/ConsultationType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Consultation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConsultationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('dateConsultation')
+            ->add('diagnostic')
+            ->add('prochainRV')
+            ->add('autre')
+            ->add('facturation')
+            ->add('patient')
+            ->add('rendezVous')
+            ->add('personnel')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Consultation::class,
+        ]);
+    }
+}
