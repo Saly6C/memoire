7304014c41b0d4771f09df94d9cad9b3ac47545b
Saly6C <salimatacisse59@gmail.com>
2019-12-20 14:19:33 +0000

    path /service changé en /dashboard/service

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/service")
+ * @Route("/dashboard/service")
  */
 class ServiceController extends AbstractController
 {
