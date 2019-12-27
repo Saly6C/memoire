a21282f826c009672b884e8f58f18b4eb83e9915
Saly6C <salimatacisse59@gmail.com>
2019-12-27 21:12:37 +0000

    type du champ dateConsultation, changé en datepicker

--- src/Form/ConsultationType.php
+++ src/Form/ConsultationType.php
@@ -6,13 +6,20 @@ use App\Entity\Consultation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class ConsultationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dateConsultation')
+            ->add('dateConsultation', DateType::class, [
+                'widget'=> 'single_text',
+                // 'placeholder' => 'Cliquez pour sélectionner une valeur',
+                'html5' => false,
+                // 'view_timezone' => 'Africa/Dakar',
+                'attr' => ['class' => 'js-datepicker']
+            ])
             ->add('diagnostic')
             ->add('prochainRV')
             ->add('autre')
