123026ff86c6fa79635b030ef36ef261cd181425
Saly6C <salimatacisse59@gmail.com>
2020-01-06 22:00:17 +0000

    5 derniers consultations, compteActif envoyés à la vue base.html.twig

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
 use App\Form\PatientType;
+use App\Repository\UserRepository;
 use App\Repository\PatientRepository;
+use App\Repository\ConsultationRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,8 +28,11 @@ class AccueilController extends AbstractController {
     /**
      * @Route("/", name= "clinic_dashboard")
      */
-    public function index() { 
-        return $this->render("/accueil/dashboard.html.twig");
+    public function index(ConsultationRepository $consultationRepository, UserRepository $compteActif): Response { 
+        return $this->render("/accueil/dashboard.html.twig", [
+            'lastConsultation' => $consultationRepository->findBy(array(), ['dateConsultation' => 'desc'] , 5, null),
+            'compteActif' => $compteActif->findBy(['status' => 'actif'],null,5, null)
+            ]);
     }
 
     
