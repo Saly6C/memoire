a7f925d4b3db3171f172abd80187709bf8374469
Saly6C <salimatacisse59@gmail.com>
2020-01-06 22:03:08 +0000

    dateRV changé en DateTimeType, pour un dateTimepicker

--- src/Form/RendezVousType.php
+++ src/Form/RendezVousType.php
@@ -6,15 +6,22 @@ use App\Entity\RendezVous;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class RendezVousType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dateRV')
+            // ->add('dateRV')
             ->add('nomClient')
             ->add('email')
+            ->add('dateRV', DateTimeType::class, [
+                'widget'=> 'single_text',
+                'html5' => false,
+                'label' => 'Date Rendez-vous',
+                'attr' => ['class' => 'js-datepicker', 'placeholder' => 'Cliquez pour choisir une date et heure'],                
+            ])
         ;
     }
 
