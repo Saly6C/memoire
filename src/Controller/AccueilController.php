69ad01c2e7bf08c112973fdcc1952c247bf2ee74
Saly6C <salimatacisse59@gmail.com>
2020-01-10 22:26:43 +0000

    requetes pour recupérer le totalPatient, totalhospitalisation/mois, totalChambre

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,11 +6,13 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\PatientType;
 use App\Entity\Consultation;
+use App\Entity\Hospitalisation;
 use Doctrine\ORM\EntityManager;
 use App\Repository\UserRepository;
 use App\Repository\PatientRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ConsultationRepository;
+use App\Repository\HospitalisationRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,24 +33,48 @@ class AccueilController extends AbstractController {
     /**
      * @Route("/", name= "clinic_dashboard")
      */
-    public function index(EntityManagerInterface $em, ConsultationRepository $consultationRepository, UserRepository $compteActif): Response { 
+    public function index(EntityManagerInterface $em, HospitalisationRepository $hr, ConsultationRepository $consultationRepository, UserRepository $compteActif): Response { 
         // $nbrConsultation = $consultationRepository->
         // $query = $em->createQuery('SELECT IDENTITY(c.personnel),COUNT(c.id) as nombre  FROM app\Entity\Consultation c GROUP BY c.personnel');
         // $nbr = $query->getResult();
 
+        // Nombre total de consultation par service
         $query = $em->createQuery('SELECT COUNT(c.id) as nbrConsultation, p.nomPersonnel, s.nomService 
                                     FROM app\Entity\Consultation c 
                                     INNER JOIN app\Entity\Personnel p
                                     INNER JOIN app\Entity\Service s
                                     WHERE c.personnel = p.id 
                                     AND s.id = p.service
-                                    GROUP BY s.nomService ');
+                                    GROUP BY s.nomService '    
+                                );
         $nbr = $query->getResult();
+
+        // Nombre total d'hospitalisations par mois
+        $req = $em->createQuery("SELECT COUNT(h.id) as nbrHospitalisation, MONTH(h.dateHospitalisation) as mois
+                                    FROM app\Entity\Hospitalisation h
+                                    GROUP BY mois"
+                                );
+        $month = $req->getResult();
+
+        // Nombre total de patients
+        $reqPatient = $em->createQuery("SELECT COUNT(p.id) as totalPatient
+                                    FROM app\Entity\Patient p"
+                                );
+        $totalPatient = $reqPatient->getResult();
+        
+        // Nombre total de chambres
+        $reqChambre = $em->createQuery("SELECT COUNT(ch.id) as totalChambre
+                                    FROM app\Entity\Chambre ch"
+                                );
+        $totalChambre = $reqChambre->getResult();
         
         return $this->render("/accueil/dashboard.html.twig", [
-            'lastConsultation' => $consultationRepository->findBy(array(), ['dateConsultation' => 'desc'] , 5, null),
+            'lastConsultation' => $consultationRepository->findBy(array(), ['dateConsultation' => 'desc'] , 3, null),
             'compteActif' => $compteActif->findBy(['status' => 'actif'],null,5, null),
-            'consultationParService' => $nbr
+            'consultationParService' => $nbr,
+            'hospitalisationParMois' => $month,
+            'totalPatient' =>  $totalPatient,
+            'totalChambre' =>  $totalChambre
             ]);
     }
 
