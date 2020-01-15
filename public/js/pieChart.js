13d6eeb846bed331a813df70cbf9677dd01e3712
Saly6C <salimatacisse59@gmail.com>
2020-01-15 23:06:22 +0000

    pieChart données dynamisées

--- public/js/pieChart.js
+++ public/js/pieChart.js
@@ -1,54 +1,7 @@
 var array = document.getElementById('arrayHospitalisation').value;
-var test1 = JSON.parse(array);
-var key;
-// switch (test1[key].mois) {
-//   case 1: 'Janvier'
-//     break;
-//   case 2: 'Février'
-//     break;
-//   case 3: 'Mars'
-//     break;
-//   case 4: 'Avril'
-//     break;
-//   case 5: 'Mai'
-//     break;
-//   case 6: 'Juin'
-//     break;
-//   case 7: 'Juillet'
-//     break;
-//   case 8: 'Août'
-//     break;
-//   case 9: 'Septembre'
-//     break;
-//   case 10: 'Octobre'
-//     break;
-//   case 11: 'Novembre'
-//     break;
-//   case 12: 'Décembre'
-//     break;
+var hospitalisationArray = JSON.parse(array);
 
-//   default:
-//     break;
-// }
-// for(key in test1) {
-//   var month ;
-//   if (key == 0 && test1[0].mois == 1) {
-//       month = "Janvier"
-//   }
-//   if (key == 0 && test1[].mois == 1) {
-//       month = "Janvier"
-//   }
-  // var month = test1[key].mois
-  // var k = key;
-  // if (  k == 0 && month == 1)
-  // {
-  //   month = "janvier"
-  // } else ( k == 1 && month == 1)
-  // {
-  //   month = "Fevrier"
-  // }
-// }
-console.log(test1)
+// console.log(array)
  // Load the Visualization API and the corechart package.
  google.charts.load('current', {'packages':['corechart']});
 
@@ -61,14 +14,17 @@ console.log(test1)
  function drawChart() {
 
    // Create the data table.
-   var data = new google.visualization.DataTable();
+   var data = new google.visualization.DataTable(hospitalisationArray);
    data.addColumn('string', 'Topping');
    data.addColumn('number', 'Slices');
-   data.addRows([
-     [test1[0].mois, parseInt(test1[0].nbrHospitalisation)],
-     [test1[1].mois, parseInt(test1[1].nbrHospitalisation)],
-   ]);
-
+   for (let index = 0; index < hospitalisationArray.length; index++) {
+    //  const element = array[index];
+      data.addRows([
+        [hospitalisationArray[index].mois, parseInt(hospitalisationArray[index].nbrHospitalisation)],
+      ]);
+     
+   }
+  
    // Set chart options
    var options = {'title':'Nombre d\'hospitalisations par mois',
                   // 'width':400,
